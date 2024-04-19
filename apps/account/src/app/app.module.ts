6942d1ceba3933e3fabe488ba31f6c9ad479c735
Artem <162103655+artem1dev@users.noreply.github.com>
2024-04-19 19:29:09 +0300

    add auth user modules

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserModule } from "./user/user.module";
+import { AuthController } from "./auth/auth.controller";
+import { AuthService } from "./auth/auth.service";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UserModule, AuthModule],
+    controllers: [AppController, AuthController],
+    providers: [AppService, AuthService],
 })
 export class AppModule {}
